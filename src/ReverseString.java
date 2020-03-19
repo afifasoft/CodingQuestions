447dac7571833f40f7e8d786519d147366e0c57f
afifasoft <abdul.pesit@gmail.com>
2020-03-19 11:47:02 +0530

    Reverse a String

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -4,14 +4,27 @@ public class ReverseString {
 	public static void main(String[] args) {
 		
 		String str = "Hello";
-		String result = reverseString(str);
+		String result = reverseWithStringBuilder(str);
 		System.out.println("result: "+result);
+		
+		String res = reverseManually(str);
+		System.out.println("reverse mannually: "+ res);
 	}
 	
 	
-	public static String reverseString(String s) {
-		StringBuilder sb = new StringBuilder(s);
-		sb.reverse();
+	private static String reverseWithStringBuilder(String s) {
+		
+		return new StringBuilder(s).reverse().toString();
+		
+		
+	}
+	
+	private static String reverseManually(String str) {
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = str.length() - 1; i >= 0; i--) {
+			sb.append(str.charAt(i));
+		}
 		
 		return sb.toString();
 	}
